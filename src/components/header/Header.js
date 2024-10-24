0633f579e6e5b5e80c561e9d9cd99025f96469af
samadhan <samadhannshejal99@gmail.com>
2024-10-25 01:53:16 +0530

    1010

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,7 @@ const Header = ({ onFileUpload }) => {
   return (
     <header className="header">
       <h1 className="header-title">Vehicle Dashboard</h1>
-      <h2 className="upload-title">Upload Excel File</h2>
+      <h2 className="upload-title">Upload Vehicle Data file </h2>
       <input type="file" onChange={onFileUpload} className="upload-input" />
     </header>
   );
